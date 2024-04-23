8895ded98323fae13988d7d123cdccf996c63f64
CasperWong-jpg <casper.wong.001@hotmail.com>
2024-04-22 22:29:59 -0400

    Updated ultrasonic code

--- rpi_pico/ultrasonic_sensing.py
+++ rpi_pico/ultrasonic_sensing.py
@@ -1,13 +1,25 @@
 from machine import Pin
 import utime
 
-trigger_in = Pin(28, Pin.OUT)
+# Define constants
+threshold = 100  # TODO: Modify depending on doorway distance
+
+# Define global variables 
+num_occupants = 0
+prev_distance_in = 0
+prev_distance_out = 0
+rising_edge_in = False
+rising_edge_out = False
+
+# Define GPIO pins
+trigger_in = Pin(28, Pin.OUT)  # Marked with clip
 echo_in = Pin(27, Pin.IN)
 
 trigger_out = Pin(26, Pin.OUT)
 echo_out = Pin(22, Pin.IN)
 
-def ultra():
+
+def ultra(trigger, echo):
     # Emit an ultrasound
     trigger.low()
     utime.sleep_us(2)
@@ -21,14 +33,38 @@ def ultra():
     while echo.value() == 1:
         signalon = utime.ticks_us()
     
-    # Compute time passed
+    # Compute time passed and distance
     timepassed = signalon - signaloff
     distance = (timepassed * 0.0343) / 2
-    print("The distance from object is ", distance, "cm")
     
+    return distance
+    
+
 while True:
-    print("Detecting ultra...")
-    ultra()
+    distance_in = ultra(trigger_in, echo_in)
+    distance_out = ultra(trigger_out, echo_out)
+    
+    if (prev_distance_in - distance_in) > threshold:
+        if rising_edge_out:
+            print("Occupant entered room")
+            num_occupants += 1
+            rising_edge_out = False
+        else:
+            rising_edge_in = True
+    if (prev_distance_out - distance_out) > threshold:
+        if rising_edge_in:
+            print("Occupant exited room")
+            num_occupants = max(num_occupants - 1, 0)
+            rising_edge_in = False
+        else:
+            rising_edge_out = True
     utime.sleep_ms(100)
+    
+    prev_distance_in = distance_in
+    prev_distance_out = distance_out
+    
+    print(f"Num occupants: {num_occupants}")
+    # print(f"distance_in: {distance_in}, distance_out: {distance_out}")
+    # print(f"rising_edge_in: {rising_edge_in}, rising_edge_out: {rising_edge_out}")
 
 
